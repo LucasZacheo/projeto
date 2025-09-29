29564b009aba1d9d509d25ad57eef06603eb6dd3
LucasZacheo <lucasgabrielzacheo@gmail.com>
2025-09-29 15:47:59 -0300

    :tada:

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  const descricao = document.querySelector("profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./fotos/oculosescuro.png")
+    descricao.setAttribute(
+      "alt",
+      "Foto de Lucas Zacheo, usando oculos escuros, fazendo joinha"
+    )
+  } else {
+    img.setAttribute("src", "./fotos/juliete.png")
+    descricao.setAttribute(
+      "alt",
+      "Foto de Lucas Zacheo, de óculos juliete, usando um moletom roxo."
+    )
+  }
+}
